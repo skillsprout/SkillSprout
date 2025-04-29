2457d51ad36ec2897a1b3caf65a1ea3f3451106d
Skill Sprout <skillsprout.kotabagh@gmail.com>
2025-04-29 19:05:29 +0530

    Update new.js

--- form/new.js
+++ form/new.js
@@ -52,10 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    let container = document.getElementById("container");
-});
-
+const container = document.querySelector('.container');
 // 700px Sing In logic
 const signUpBtns = document.querySelectorAll('#signUp'); 
 signUpBtns.forEach((btn) => {
